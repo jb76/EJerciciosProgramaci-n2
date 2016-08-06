d6db07109ed449ac27ed7f3f74e2646a3874f429
jb76 <jorgeb1976@outlook.com>
2016-08-06 11:55:16 -0600

    FraseInvertida

---
+++ FraseInvertidad/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FraseInvertida
+    {
+    class Program
+    {
+        public static string Mid(string param, int startIndex, int length)
+        {
+            string result = param.Substring(startIndex, length);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            string NOM;
+            string AUX;
+            int POS = 0;
+            AUX = "";
+            Console.Write("DIGITE UNA FRASE");
+            NOM = Console.ReadLine();
+            POS = NOM.Length;
+            while ((POS > 0))
+            {
+                AUX = AUX + Mid(NOM, POS - 1, 1);
+                POS = POS - 1;
+            }
+            Console.WriteLine("FRASE INVERTIDA :" + AUX);
+            Console.Write("Pulse una tecla:");
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
