e1567dff390a0f99da5bcfd14922b717335274d1
jb76 <jorgeb1976@outlook.com>
2016-08-06 12:00:18 -0600

    NumeroVocal

---
+++ NumeroVocal/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroVocal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string letra;
+            uint a = 0, e = 0, i = 0, o = 0, u = 0;
+
+            Console.Write("INGRESE UNA FRASE:");
+            letra = Console.ReadLine();
+            for (int x = 0; x < 20; x++)
+            {
+                if (letra[x] == 'a')
+                    a++;
+                if (letra[x] == 'e')
+                    e++;
+                if (letra[x] == 'i')
+                    i++;
+                if (letra[x] == 'o')
+                    o++;
+                if (letra[x] == 'u')
+                    u++;
+            }
+            Console.WriteLine("a = {0}, e = {1}, i = {2}, o = {3}, u = {4}", a, e, i, o, u);
+            Console.Read();
+
+        }
+    }
+}
