f114836f5f11f2e7839b7dab0a1005e413ed3cc3
jb76 <jorgeb1976@outlook.com>
2016-08-06 12:02:06 -0600

    PalabraMasLarga

---
+++ PalabraMasLarga/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalabraMasLarga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra, palabra_mas_larga = "";
+            while (true)
+            {
+                Console.Write("Ingrese una Palabra: ");
+                palabra = Console.ReadLine();
+
+                if (palabra == "")
+                    break;
+                if (palabra.Length > palabra_mas_larga.Length)
+                    palabra_mas_larga = palabra;
+            }
+            Console.WriteLine(" La palabra más larga fue: {0}", palabra_mas_larga);
+            Console.ReadLine();
+
+        }
+    }
+}
